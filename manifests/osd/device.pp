7b8d80ef30e37f3c46ba674fa7a1f09fdac683ec
Oskar Stephens <oskars@darkhorse.com>
2014-03-20 15:50:04 -0700

    Add ceph-disk activate-key option to osd

--- manifests/osd/device.pp
+++ manifests/osd/device.pp
@@ -9,7 +9,7 @@
 #   Filesystem type used to format the OSD
 #
 # [*cluster_name*]
-#   Name of the cluster to join
+#   Name of the cluster to join, default is ceph
 #
 # [*cluster_uuid*]
 #   UUID of the cluster to join
@@ -17,6 +17,9 @@
 # [*journal_path*]
 #   Path used to store the journal for this OSD.
 #   If not specified, it will be on the same disk as the OSD data partition
+#
+# [*bootstrap_key*]
+#   Location of the auth key to use for joining the cluster
 #
 # == Dependencies
 #
@@ -33,9 +36,10 @@
 
 define ceph::osd::device (
   $fs_type = 'xfs',
-  $cluster_name,
-  $cluster_uuid,
+  $cluster_name = undef,
+  $cluster_uuid = undef,
   $journal_path = undef,
+  $bootstrap_key = undef,
 ) {
 
   include ceph::osd
@@ -50,13 +54,17 @@ define ceph::osd::device (
     $cluster_uuid_option = "--cluster-uuid ${cluster_uuid}"
   }
 
+  if $bootstrap_key {
+    $bootstrap_key_option = "--activate-key ${bootstrap_key}"
+  }
+
   exec { "ceph_prepare_${name}":
     command => "ceph-disk prepare ${cluster_name_option} ${cluster_uuid_option} --fs-type ${fs_type} --zap-disk ${name} ${journal_path}",
     unless  => "ceph-disk list | grep -E \"${name}[0-9]+ ceph data, prepared\"",
     require => Package['btrfs-tools', 'xfsprogs'],
   } ->
   exec { "ceph_activate_${name}":
-    command => "ceph-disk activate ${name}",
+    command => "ceph-disk activate ${name} ${bootstrap_key_option}",
     unless  => "ceph-disk list | grep -E \"${name}[0-9]+ ceph data, prepared\"",
   }
 
